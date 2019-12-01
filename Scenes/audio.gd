32f5797cbe7c133ad27782d09372440218cc1e07
eric <47113448+funclubfuntime@users.noreply.github.com>
2019-12-01 21:11:32 +0100

    Final commit

--- Scenes/audio.gd
+++ Scenes/audio.gd
@@ -9,28 +9,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print(AudioServer.is_bus_mute(sfx))
+#	print(AudioServer.is_bus_mute(sfx))
 	pass
-
-
-func _on_CheckBox_button_up():
-	AudioServer.set_bus_mute(music, true)
-	pass # Replace with function body.
-
-
-func _on_CheckBox_button_down():
-	AudioServer.set_bus_mute(music, false)
-	pass # Replace with function body.
-
-
-func _on_CheckBox2_button_down():
-	AudioServer.set_bus_mute(sfx, false)
-	pass # Replace with function body.
-
-
-func _on_CheckBox2_button_up():
-	AudioServer.set_bus_mute(sfx, true)
-	pass # Replace with function body.
 
 
 func _on_CheckBox_toggled(button_pressed):
