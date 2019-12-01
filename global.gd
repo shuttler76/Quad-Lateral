6d2e2d12af69be4243ecd9764cf97ee4d0537640
eric <47113448+funclubfuntime@users.noreply.github.com>
2019-12-01 18:25:44 +0100

    Scene change bug fix

--- global.gd
+++ global.gd
@@ -1,12 +1,12 @@
 extends Node
 
 var jumpMax = 0
-var prevZone = 0
+var prevZone = 1
 var currentZone = 0
-var point = Vector2(0,198)
+var point
 var player = null
 
 func _process(delta):
 	if (player != null && prevZone != currentZone):
-#		player.position = point
+		player.position = point
 		prevZone = currentZone
